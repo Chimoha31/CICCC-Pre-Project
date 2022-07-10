8ac3d12c3555ae7ca6603ab5d67a773c6bad7a51
Chimoha31 <mokochii1108@gmail.com>
2022-07-10 12:57:07 -0700

    Done :clap:

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -17,6 +17,8 @@ export const selectedReducer = (state = {}, { type, payload }) => {
   switch (type) {
     case ActionTypes.SELECTED_PRODUCT:
       return { ...state, ...payload };
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
     default:
       return state;
   }
